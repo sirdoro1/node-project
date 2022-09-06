e03cbc2efcfb32ef8edd224ebc4ab9df774beb5e
Doro <sirdoro1@gmail.com>
2022-09-06 12:14:43 +0100

    update on reading file and outputing content

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 var http = require('http');
 const {dateTime} = require('./modules/date');
 var url = require('url');
+var fs = require('fs');
 
 var server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    // Hello world
-    res.write('Hello World');
-    res.write("\r");
-    // Importing custom module
-    res.write("The date and time are currently: " + dateTime());
-    res.write("\r");
-    // getting the url
-    res.write('url: '+req.url);
-    // getting url parameters
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.write("\r");
-    res.write(txt);
-    res.end();
+    // res.writeHead(200,{'Content-Type':'text/plain'});
+    // // Hello world
+    // res.write('Hello World');
+    // res.write("\r");
+    // // Importing custom module
+    // res.write("The date and time are currently: " + dateTime());
+    // res.write("\r");
+    // // getting the url
+    // res.write('url: '+req.url);
+    // // getting url parameters
+    // var q = url.parse(req.url, true).query;
+    // var txt = q.year + " " + q.month;
+    // res.write("\r");
+    // res.write(txt);
+    // res.end();
+
+    // open file and output content
+    fs.readFile('demofile.html',function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    });
 });
 
 server.listen('5000');
