df76f7425e6c6783a1c4013576ac4cfa198d70b0
Doro <sirdoro1@gmail.com>
2022-09-06 01:08:23 +0100

    update on exploring created module

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var http = require('http');
+const {dateTime} = require('./modules/date');
 
 var server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.writeHead(301,{'Content-Type':'text/plain'});
+    res.write("The date and time are currently: " + dateTime());
+    // break new line
+    res.write("\r");
     res.end('Hello World');
 });
 
