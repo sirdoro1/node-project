6236ae2422c3d0cbfb09e395b89f518a44cc1fec
Doro <sirdoro1@gmail.com>
2022-09-06 11:20:37 +0100

    update on index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var http = require('http');
 const {dateTime} = require('./modules/date');
+var url = require('url');
 
 var server = http.createServer((req,res)=>{
-    res.writeHead(301,{'Content-Type':'text/plain'});
-    res.write("The date and time are currently: " + dateTime());
-    // break new line
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    // Hello world
+    res.write('Hello World');
     res.write("\r");
-    res.end('Hello World');
+    // Importing custom module
+    res.write("The date and time are currently: " + dateTime());
+    res.write("\r");
+    // getting the url
+    res.write('url: '+req.url);
+    // getting url parameters
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.write("\r");
+    res.write(txt);
+    res.end();
 });
 
 server.listen('5000');
